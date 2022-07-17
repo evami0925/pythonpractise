a4b70555ca2e10dbe7208cd2b28925a722710fbe
evami0925 <mijiaoqiao@gmail.com>
2022-07-17 14:58:41 +1000

    string knowledge-1

---
+++ string_practise.py
@@ -0,0 +1,36 @@
+'hello world'.lower()
+'hello world, today is awesome!'.lower()
+' hello people'.lower()
+
+'hello'+''+'world'+"!"
+
+
+'hello world!'.replace('hello','')
+
+'Kelly likes Stu?'.replace('likes','does not like')
+
+
+' Weather is good! '.lstrip()
+
+weather_string = ' Weather is good! '
+weather_string
+weather_string.title()
+weather_string.capitalize()
+weather_string.upper()
+weather_string.strip()
+
+someones_name='Stu'
+f'{someones_name.upper()} says "{weather_string}"'
+
+var_1 = "kellys says good{0}"
+var_1.format("!")
+
+
+'kelly says {0}{1}{2}'.format('hello',' ','world!')
+'kelly says {weather}'.format(weather='raining!')
+len(' kelly '.strip())
+
+
+test_str ='abcdefg'
+test_str[-1]
+
